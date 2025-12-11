6c7fcf7082dae98c06507a11978c966ab5f93202
Andre Harms <andre.harms@stackit.cloud>
2025-12-11 17:57:05 +0100

    fix: update API client references to use sqlserverflexalpha package

--- stackit/internal/services/sqlserverflexalpha/utils/util.go
+++ stackit/internal/services/sqlserverflexalpha/utils/util.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/stackitcloud/stackit-sdk-go/services/sqlserverflex"
+	"github.com/stackitcloud/terraform-provider-stackit/pkg/sqlserverflexalpha"
 
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/stackitcloud/stackit-sdk-go/core/config"
@@ -12,19 +12,34 @@ import (
 	"github.com/stackitcloud/terraform-provider-stackit/stackit/internal/utils"
 )
 
-func ConfigureClient(ctx context.Context, providerData *core.ProviderData, diags *diag.Diagnostics) *sqlserverflex.APIClient {
+func ConfigureClient(
+	ctx context.Context,
+	providerData *core.ProviderData,
+	diags *diag.Diagnostics,
+) *sqlserverflexalpha.APIClient {
 	apiClientConfigOptions := []config.ConfigurationOption{
 		config.WithCustomAuth(providerData.RoundTripper),
 		utils.UserAgentConfigOption(providerData.Version),
 	}
 	if providerData.SQLServerFlexCustomEndpoint != "" {
-		apiClientConfigOptions = append(apiClientConfigOptions, config.WithEndpoint(providerData.SQLServerFlexCustomEndpoint))
+		apiClientConfigOptions = append(
+			apiClientConfigOptions,
+			config.WithEndpoint(providerData.SQLServerFlexCustomEndpoint),
+		)
 	} else {
 		apiClientConfigOptions = append(apiClientConfigOptions, config.WithRegion(providerData.GetRegion()))
 	}
-	apiClient, err := sqlserverflex.NewAPIClient(apiClientConfigOptions...)
+	apiClient, err := sqlserverflexalpha.NewAPIClient(apiClientConfigOptions...)
 	if err != nil {
-		core.LogAndAddError(ctx, diags, "Error configuring API client", fmt.Sprintf("Configuring client: %v. This is an error related to the provider configuration, not to the resource configuration", err))
+		core.LogAndAddError(
+			ctx,
+			diags,
+			"Error configuring API client",
+			fmt.Sprintf(
+				"Configuring client: %v. This is an error related to the provider configuration, not to the resource configuration",
+				err,
+			),
+		)
 		return nil
 	}
 
