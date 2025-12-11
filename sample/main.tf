a4443df6c33188cc87515bb768c0bdc1a2462807
Andre Harms <andre.harms@stackit.cloud>
2025-12-11 19:46:47 +0100

    chore: add stackit_sqlserverflexalpha_user resource and instance_id variable

--- sample/main.tf
+++ sample/main.tf
@@ -1,1 +1,8 @@
 # see other files
+
+resource "stackit_sqlserverflexalpha_user" "ptlsdbuser" {
+  project_id  = stackitalpha_postgresflexalpha_instance.ptlsdbsrv.project_id
+  instance_id = stackitalpha_postgresflexalpha_instance.ptlsdbsrv.id
+  username    = var.db_username
+  roles       = ["login"]
+}
