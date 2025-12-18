a5b221a62599c92911abc8e63b48020c0963408e
Marcel S. Henselin <marcel.henselin@stackit.cloud>
2025-12-18 09:24:30 +0100

    fix: docs CI scripts

--- scripts/tfplugindocs.sh
+++ scripts/tfplugindocs.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 ROOT_DIR=$(git rev-parse --show-toplevel)
 EXAMPLES_DIR="${ROOT_DIR}/examples"
-PROVIDER_NAME="stackit"
+PROVIDER_NAME="stackitprivatepreview"
 
 # Create a new empty directory for the docs
 if [ -d ${ROOT_DIR}/docs ]; then
@@ -14,4 +14,4 @@ mkdir -p ${ROOT_DIR}/docs
 
 echo ">> Generating documentation"
 tfplugindocs generate \
-    --provider-name "stackit"
+    --provider-name "stackitprivatepreview"
