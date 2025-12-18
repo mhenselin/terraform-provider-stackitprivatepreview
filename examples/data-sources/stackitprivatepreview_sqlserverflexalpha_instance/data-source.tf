56b483d8caa57919fff81c09b1353bff118cf109
Marcel S. Henselin <marcel.henselin@stackit.cloud>
2025-12-18 09:48:46 +0100

    fix: adjust examples

---
+++ examples/data-sources/stackitprivatepreview_sqlserverflexalpha_instance/data-source.tf
@@ -0,0 +1,4 @@
+data "stackitprivatepreview_sqlserverflexalpha_instance" "example" {
+  project_id  = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  instance_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+}
---
+++ examples/data-sources/stackitprivatepreview_sqlserverflexalpha_user/data-source.tf
@@ -0,0 +1,5 @@
+data "stackitprivatepreview_sqlserverflexalpha_user" "example" {
+  project_id  = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  instance_id = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+  user_id     = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+}
